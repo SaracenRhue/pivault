b65ec06d006a06f71a96f35ff56d2c561056f5c0
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2024-05-22 20:23:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ sudo apt update && sudo apt upgrade -y
 sudo apt install -y htop
 curl -fsSL https://tailscale.com/install.sh | sh
 wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install && chmod +x install && sudo ./install -n
-sudo tailscale up —ssh
