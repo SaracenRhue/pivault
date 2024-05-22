868662222b938b4b2c30324b08b25eb48eda4331
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2024-05-22 20:19:34 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y && sudo apt install -y htop && curl -fsSL https://tailscale.com/install.sh | sh && wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install && chmod +x install && sudo ./install -n && sudo tailscale up —ssh
-
-# OpenMediaVault
-# Username: admin
-# Password: openmediavault
-
-curl -sL https://install.raspap.com | bash
-# IP address: 10.3.141.1
-# Username: admin
-# Password: secret
-# DHCP range: 10.3.141.50 to 10.3.141.254
-# SSID: raspi-webgui
-# Password: ChangeMe
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y htop
+curl -fsSL https://tailscale.com/install.sh | sh
+wget https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install && chmod +x install && sudo ./install -n
+sudo tailscale up —ssh
